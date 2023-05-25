1c2fe3b2f8958b5c9d759ab1d523075de6ecfd27
Kamil <mentos.96@o2.pl>
2023-05-25 22:01:54 +0200

    Write class JewlThief

---
+++ JewelThief.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_better_understanding_of_class_overwriting
+{
+    internal class JewelThief: Locksmith
+    {
+        private string stolenJewels;
+
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"Kradne oszczednosci! Ukradlem {stolenJewels}");
+
+        }
+
+    }
+}

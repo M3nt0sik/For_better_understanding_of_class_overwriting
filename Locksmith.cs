c61412da0f4254873e18c150144e0990f6b548a3
Kamil <mentos.96@o2.pl>
2023-05-25 21:54:50 +0200

    Write class Locksmith

---
+++ Locksmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_better_understanding_of_class_overwriting
+{
+    internal class Locksmith
+    {
+        public void OpenSave(Safe safe)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+
+        public string? Combination { private get; set; }
+
+        protected void ReturnContents(string safecontent, SafeOwner owner)
+        {
+            owner.ReciveContents(safecontent);
+        }
+    }
+}

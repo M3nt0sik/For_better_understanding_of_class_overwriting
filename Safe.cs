c94485a4639e998de37ea5378e2f98eb392528c2
Kamil <mentos.96@o2.pl>
2023-05-25 21:40:14 +0200

    Write class Save

---
+++ Safe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_better_understanding_of_class_overwriting
+{
+    internal class Safe
+    {
+        private string content = "cenne klejnpty";
+        private string saveCombination = "12345";
+
+        public string Open(string combination)
+        {
+            if(combination == saveCombination)
+            {
+                return content;
+            }
+            return "";
+        }
+
+        public void PickLock(Locksmith lockpicker) 
+        {
+            lockpicker.Combination = saveCombination;
+        }
+    }
+}

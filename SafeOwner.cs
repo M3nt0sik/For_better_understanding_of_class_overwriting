493d77a094bda92d50a8fac30bd3e62c254fdbd5
Kamil <mentos.96@o2.pl>
2023-05-25 21:45:57 +0200

    Write class SafeOwner

---
+++ SafeOwner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_better_understanding_of_class_overwriting
+{
+    internal class SafeOwner
+    {
+        private string valuables = "";
+
+        public void ReciveContents(string safeContents)
+        {
+            valuables = safeContents; ;
+            Console.Write($"Moje {valuables} Dziekuje za zwrot!");
+        }
+    }
+}
